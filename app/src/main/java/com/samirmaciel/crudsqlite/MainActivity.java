89390194a68e5d1afe582bfc0b3ad7e32c1877f8
Samir Maciel <samir.maciel17@gmail.com>
2021-03-08 19:39:19 -0300

    Menu inflado na Action bar!

--- app/src/main/java/com/samirmaciel/crudsqlite/MainActivity.java
+++ app/src/main/java/com/samirmaciel/crudsqlite/MainActivity.java
@@ -2,7 +2,11 @@ package com.samirmaciel.crudsqlite;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater i = getMenuInflater();
+        i.inflate(R.menu.menu_principal, menu);
+
+        MenuItem cadastrar = (MenuItem) menu.findItem(R.id.menuCadastrar);
+        cadastrar.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                Intent intent = new Intent(MainActivity.this, CadastroActivity.class);
+                startActivity(intent);
+                return false;
+            }
+        });
+
+        return true;
     }
 }
