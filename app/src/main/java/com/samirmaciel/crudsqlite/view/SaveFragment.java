09bf23608b91af46673f3c41e2b9568a33991522
Samir Maciel <samir.maciel17@gmail.com>
2021-05-17 20:35:39 -0300

    tela cadastro atualizada

---
+++ app/src/main/java/com/samirmaciel/crudsqlite/view/SaveFragment.java
@@ -0,0 +1,30 @@
+package com.samirmaciel.crudsqlite.view;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.samirmaciel.crudsqlite.R;
+
+
+public class SaveFragment extends Fragment {
+
+    public SaveFragment() {
+        // Required empty public constructor
+    }
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_save, container, false);
+
+
+        return view;
+    }
+}
